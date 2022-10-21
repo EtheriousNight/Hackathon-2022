4b467951a0e40507a9059605aa20827edae93753
Richard <richardmrosk@gmail.com>
2022-10-21 16:12:52 +0200

    Init Springboot

---
+++ backend/src/test/java/com/hackathon2022/demo/Hackathon2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon2022.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
