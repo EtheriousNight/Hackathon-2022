b56023263b3337d09b57a983b0f9b449c50caf6a
MrRade <domri.rade.ms@gmail.com>
2022-10-22 13:23:37 +0200

    created input

--- Frontend/src/pages/StartPage.tsx
+++ Frontend/src/pages/StartPage.tsx
@@ -1,7 +1,28 @@
-export const StartPage = () => {
-    return(
-        <div>
+import {Button, TextField} from "@mui/material";
+import {useState} from "react";
 
+export const StartPage = () => {
+    const [street, setStreet] = useState<string>("");
+    const [name, setName] = useState<string>("");
+
+    const onSubmit = () => setName(street);
+
+    return (
+        <div>
+            <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue={street}
+                onChange={(e: any) => setStreet(e.target.value)}
+            />
+            <Button
+                variant={"contained"}
+                onClick={onSubmit}
+            >
+                submit
+            </Button>
+            {name}
         </div>
-        )
+    )
 }
