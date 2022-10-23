334d9b53545f0959eba321081d8b638c3dd9fcf0
Christopher <hilgi45@gmail.com>
2022-10-23 12:10:50 +0200

    Zeug hochschieben.

---
+++ svelte-frontend/src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import EventList from "./lib/EventList.svelte";
+    import SearchBar from "./lib/SearchBar.svelte";
+</script>
+
+<main>
+    <div class="row">
+        <div class="col s1" />
+        <div class="col s10">
+            <SearchBar />
+            <EventList />
+        </div>
+        <div class="col s1" />
+    </div>
+    
+</main>
+
+<style>
+    .row {
+        margin-top: 25px;
+    }
+</style>
